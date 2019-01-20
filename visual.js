cd9f8ec37151be527f73c8265448ff31d9111237
DannyS712 <DannyS712.enwiki@gmail.com>
2019-01-19 22:59:15 -0800

    get_chosen_ns_s function
    separate detecting which namespaces have been chosen to another function, use dictionary to log namespace number, instead of name

--- visual.js
+++ visual.js
@@ -174,37 +174,73 @@ function tagsmanager() {
 		$("#CL-next").click(function(){
 			var chosen_category = $('#CL-cat-name').val();
 			console.log( chosen_category );
-			if ($("#CL-option-checkbox-Article").prop("checked")) console.log("Articles");
-			if ($("#CL-option-checkbox-Talk").prop("checked")) console.log("Talk");
-			if ($("#CL-option-checkbox-User").prop("checked")) console.log("User");
-			if ($("#CL-option-checkbox-User_talk").prop("checked")) console.log("User talk");
-			if ($("#CL-option-checkbox-Wikipedia").prop("checked")) console.log("Wikipedia");
-			if ($("#CL-option-checkbox-Wikipedia_talk").prop("checked")) console.log("Wikipedia talk");
-			if ($("#CL-option-checkbox-File").prop("checked")) console.log("File");
-			if ($("#CL-option-checkbox-File_talk").prop("checked")) console.log("File talk");
-			if ($("#CL-option-checkbox-MediaWiki").prop("checked")) console.log("MediaWiki");
-			if ($("#CL-option-checkbox-MediaWiki_talk").prop("checked")) console.log("MediaWiki talk");
-			if ($("#CL-option-checkbox-Template").prop("checked")) console.log("Template");
-			if ($("#CL-option-checkbox-Template_talk").prop("checked")) console.log("Template talk");
-			if ($("#CL-option-checkbox-Help").prop("checked")) console.log("Help");
-			if ($("#CL-option-checkbox-Help_talk").prop("checked")) console.log("Help talk");
-			if ($("#CL-option-checkbox-Category").prop("checked")) console.log("Category");
-			if ($("#CL-option-checkbox-Category_talk").prop("checked")) console.log("Category talk");
-			if ($("#CL-option-checkbox-Portal").prop("checked")) console.log("Portal");
-			if ($("#CL-option-checkbox-Portal_talk").prop("checked")) console.log("Portal talk");
-			if ($("#CL-option-checkbox-Book").prop("checked")) console.log("Book");
-			if ($("#CL-option-checkbox-Book_talk").prop("checked")) console.log("Book talk");
-			if ($("#CL-option-checkbox-Draft").prop("checked")) console.log("Draft");
-			if ($("#CL-option-checkbox-Draft_talk").prop("checked")) console.log("Draft talk");
-			if ($("#CL-option-checkbox-TimedText").prop("checked")) console.log("TimedText");
-			if ($("#CL-option-checkbox-TimedText_talk").prop("checked")) console.log("TimedText talk");
-			if ($("#CL-option-checkbox-Module").prop("checked")) console.log("Module");
-			if ($("#CL-option-checkbox-Module_talk").prop("checked")) console.log("Module talk");
-			if ($("#CL-option-checkbox-Gadget").prop("checked")) console.log("Gadget");
-			if ($("#CL-option-checkbox-Gadget_talk").prop("checked")) console.log("Gadget talk");
-			if ($("#CL-option-checkbox-Gadget_Definition").prop("checked")) console.log("Gadget Definition");
-			if ($("#CL-option-checkbox-Gadget_Definition_talk").prop("checked")) console.log("Gadget Definition talk");
+
 			$("#CL-modal").remove();
 		});
 	};
+}
+function get_chosen_ns_s(){
+	var ns_dict = {
+  		"Article": 0,
+  		"Talk": 1,
+  		"User": 2,
+  		"User talk": 3,
+  		"Wikipedia": 4,
+  		"Wikipedia talk": 5,
+  		"File": 6,
+  		"File talk": 7,
+  		"MediaWiki": 8,
+  		"MediaWiki talk": 9,
+  		"Template": 10,
+  		"Template talk": 11,
+  		"Help": 12,
+  		"Help talk": 13,
+  		"Category": 14,
+  		"Category talk": 15,
+  		"Portal": 100,
+  		"Portal talk": 101,
+  		"Book": 108,
+  		"Book talk": 109,
+  		"Draft": 118,
+  		"Draft talk": 119,
+  		"TimedText": 710,
+  		"TimedText talk": 711,
+  		"Module": 828,
+  		"Module talk": 829,
+  		"Gadget": 2300,
+  		"Gadget talk": 2301,
+  		"Gadget Definition": 2302,
+  		"Gadget Definition talk": 2303
+	};
+
+	if ($("#CL-option-checkbox-Article").prop("checked")) console.log(ns_dict["Article"]);
+	if ($("#CL-option-checkbox-Talk").prop("checked")) console.log(ns_dict["Talk"]);
+	if ($("#CL-option-checkbox-User").prop("checked")) console.log(ns_dict["User"]);
+	if ($("#CL-option-checkbox-User_talk").prop("checked")) console.log(ns_dict["User talk"]);
+	if ($("#CL-option-checkbox-Wikipedia").prop("checked")) console.log(ns_dict["Wikipedia"]);
+	if ($("#CL-option-checkbox-Wikipedia_talk").prop("checked")) console.log(ns_dict["Wikipedia talk"]);
+	if ($("#CL-option-checkbox-File").prop("checked")) console.log(ns_dict["File"]);
+	if ($("#CL-option-checkbox-File_talk").prop("checked")) console.log(ns_dict["File talk"]);
+	if ($("#CL-option-checkbox-MediaWiki").prop("checked")) console.log(ns_dict["MediaWiki"]);
+	if ($("#CL-option-checkbox-MediaWiki_talk").prop("checked")) console.log(ns_dict["MediaWiki talk"]);
+	if ($("#CL-option-checkbox-Template").prop("checked")) console.log(ns_dict["Template"]);
+	if ($("#CL-option-checkbox-Template_talk").prop("checked")) console.log(ns_dict["Template talk"]);
+	if ($("#CL-option-checkbox-Help").prop("checked")) console.log(ns_dict["Help"]);
+	if ($("#CL-option-checkbox-Help_talk").prop("checked")) console.log(ns_dict["Help talk"]);
+	if ($("#CL-option-checkbox-Category").prop("checked")) console.log(ns_dict["Category"]);
+	if ($("#CL-option-checkbox-Category_talk").prop("checked")) console.log(ns_dict["Category talk"]);
+	if ($("#CL-option-checkbox-Portal").prop("checked")) console.log(ns_dict["Portal"]);
+	if ($("#CL-option-checkbox-Portal_talk").prop("checked")) console.log(ns_dict["Portal talk"]);
+	if ($("#CL-option-checkbox-Book").prop("checked")) console.log(ns_dict["Book"]);
+	if ($("#CL-option-checkbox-Book_talk").prop("checked")) console.log(ns_dict["Book talk"]);
+	if ($("#CL-option-checkbox-Draft").prop("checked")) console.log(ns_dict["Draft"]);
+	if ($("#CL-option-checkbox-Draft_talk").prop("checked")) console.log(ns_dict["Draft talk"]);
+	if ($("#CL-option-checkbox-TimedText").prop("checked")) console.log(ns_dict["TimedText"]);
+	if ($("#CL-option-checkbox-TimedText_talk").prop("checked")) console.log(ns_dict["TimedText talk"]);
+	if ($("#CL-option-checkbox-Module").prop("checked")) console.log(ns_dict["Module"]);
+	if ($("#CL-option-checkbox-Module_talk").prop("checked")) console.log(ns_dict["Module talk"]);
+	if ($("#CL-option-checkbox-Gadget").prop("checked")) console.log(ns_dict["Gadget"]);
+	if ($("#CL-option-checkbox-Gadget_talk").prop("checked")) console.log(ns_dict["Gadget talk"]);
+	if ($("#CL-option-checkbox-Gadget_Definition").prop("checked")) console.log(ns_dict["Gadget Definition"]);
+	if ($("#CL-option-checkbox-Gadget_Definition_talk").prop("checked")) console.log(ns_dict["Gadget Definition talk"]);
 }
